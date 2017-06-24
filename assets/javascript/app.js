357ff46ef62af12d4c58054e9f0acc05b92360fb
Jonathan Wong <jdwong2@yahoo.com>
2017-06-23 20:30:07 -0700

    Add files

---
+++ assets/javascript/app.js
@@ -0,0 +1,73 @@
+var topics=["dog","cat","turtle","cow","horse","elephant","lion"];
+
+function renderButtons() {
+  $("#button-holder").empty()
+  for (var i=0; i < topics.length; i++) {
+    var newButton = $("<button>");
+    newButton.text(topics[i]);
+    newButton.attr("type", "button");
+    newButton.attr("class", "animals");
+    $("#button-holder").append(newButton);
+  }
+}
+
+$("#add-animal").on("click", function(event) {
+  event.preventDefault();
+  
+  var animal = $("#animal-input").val().trim();
+  topics.push(animal);
+  renderButtons();
+});
+
+$(document).on("click", ".animals", displayGifs);
+
+$(document).on("click", ".animal-image", function () {
+
+  if ($(this).attr("state") === "still") {
+    $(this).attr("src", $(this).attr("data-animate"));
+    $(this).attr("state", "animate");
+  }
+  else {
+    $(this).attr("src", $(this).attr("data-still"));
+    $(this).attr("state", "still");
+  }
+  
+});
+
+function displayGifs() {
+  var q = $(this).text();
+
+  var queryURL="http://api.giphy.com/v1/gifs/search?q=" +
+    q + "&api_key=dc6zaTOxFJmzC&limit=10";
+
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).done(function(response) {
+
+    var data = response.data;
+    console.log(data);
+    $("#gif-holder").empty();
+    for (var i = 0; i < data.length; i++) {
+      var newDiv = $("<div>");
+      newDiv.css("display", "inline-block");
+      newDiv.css("margin", "0px 20px 10px 0px");
+      var p = $("<p>");
+      var str = "Rating: " + data[i].rating.toUpperCase();
+      p.text(str);
+      newDiv.append(p);
+      var gif = $("<img>");
+      gif.attr("src", data[i].images.fixed_height_still.url);
+      gif.attr("data-still", data[i].images.fixed_height_still.url);
+      gif.attr("data-animate", data[i].images.fixed_height.url)
+      gif.attr("state", "still");
+      gif.addClass("animal-image");
+      newDiv.append(gif);
+      $("#gif-holder").append(newDiv);
+    }
+
+  });
+
+}
+
+renderButtons();
